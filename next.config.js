002d3149960cc076fc4738f998c4500cc84a073d
heavenstar0531 <jackfortin0220@gmail>
2023-08-15 16:11:53 -0400

    deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    OPENAI_API_KEY: "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",
-  },
 };
 
 module.exports = nextConfig;
